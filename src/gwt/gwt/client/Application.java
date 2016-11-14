0541d0b26efa49caa731ee97ad0d3a7da199c9fb
mhuang <mhuang@kobo.com>
2016-11-14 12:59:39 -0500

    Initial commit

---
+++ src/gwt/gwt/client/Application.java
@@ -0,0 +1,14 @@
+package gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application implements EntryPoint {
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+    }
+}
